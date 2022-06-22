5d6b8053c202caae328419d1f32c62d1c952668e
Indra Marasovich <indramarasovich>
2022-06-21 23:30:59 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,57 +1,86 @@
-let opcion;
+function dos() {
+    let resultado = (4 * 300)*0.90;
+    return resultado;
+  }
+  
+function cuatro() {
+    let resultado = (12 * 300)*0.85;
+    return resultado;
+  }
 
-alert("Bienvenido a ¿Qué comemos hoy?");
-let usuario = prompt("Elegí tu usuario: (1: Juliana; 2: Lucas)");
+function ocho() {
+    let resultado = (24 * 300)*0.80;
+    return resultado;
+  }
+  
+function inicio() {
+    let opcion = prompt("¡Bienvenido a Meta Movimiento! \nConocé los planes:\n 1. Pack x2 semanas\n 2. Pack x4 semanas\n 3. Pack x8 semanas\n 4. Siguiente paso");
+    return opcion;
+  }
 
-if(usuario == 1) {
+function pago() {
+    let opcion = prompt("Ahora que conoces todos los planes, elegí el que más te guste para saber el precio promocional \n 1. Pack x2 semanas\n 2. Pack x4 semanas\n 3. Pack x8 semanas\n 4. EXIT");
+    return opcion;
+  }
 
-    opcion = prompt("Hola Juliana! Ingresa un día de la semana (lunes a domingo) | para salir escribir EXIT: ");
 
-    while(opcion != "EXIT") {
-        switch(opcion) {
-            case "lunes":
-                alert("Hoy comes hamburguesas");
+  
+function metaMovimiento() {
+    let plan = inicio();
+    plan = parseInt(plan)
+
+    while (plan !== 4) {
+        switch (plan) {
+            case 1:
+                alert("El pack x2 semanas cuenta con 10% de descuento!\n- 2 clases por semana \n- Trabajaremos flexibilidad y aeróbico \n- El precio por clase es de $300");
                 break;
-            case "martes":
-                alert("Hoy comes panchos");
+
+            case 2:
+                alert("El pack x4 semanas cuenta con 15% de descuento!\n- 3 clases por semana \n- Trabajaremos flexibilidad, resistencia, aeróbico y fuerza\n- El precio por clase es $300");
                 break;
-            case "miercoles":
-                alert("Hoy comes empanadas");
+  
+            case 3:
+                alert("El pack x8 semanas cuenta con 20% de descuento!\n- 3 clases por semana \n- Trabajaremos flexibilidad, resistencia, aeróbico, fuerza y estabilidad \n- El precio por clase es $300");
                 break;
-            case "jueves":
-                alert("Hoy comes pizzas");
-                break;
+  
             default:
-                alert("viernes, sabado o domingo menú libre!")
+                alert("Opcion no válida");
                 break;
-        }
-        opcion = prompt("Ingrese día de la semana (lunes a domingo)| para salir escribir EXIT");
+         }
+        
+         plan = inicio();
+         plan = parseInt(plan)
     }
-}else if(usuario == 2) {
-    opcion = prompt("Hola Lucas! Ingrese día de la semana (lunes a domingo) | para salir escribir EXIT: ");
 
-    while(opcion != "EXIT") {
-        switch(opcion) {
-            case "lunes":
-                alert("Hoy comes sopa");
+    let total = pago();
+    total = parseFloat(total)
+
+    while (total !== 4) {
+        switch (total) {
+            case 1:
+                precio = dos()
+                alert("El precio es: $" + precio)
                 break;
-            case "martes":
-                alert("Hoy comes ravioles");
+
+            case 2:
+                precio = cuatro()
+                alert("El precio es: $" + precio)
                 break;
-            case "miercoles":
-                alert("Hoy comes ensalada");
+  
+            case 3:
+                precio = ocho()
+                alert("El precio es: $" + precio)
                 break;
-            case "jueves":
-                alert("Hoy comes panchos");
-                break;
+  
             default:
-                alert("viernes, sabado o domingo menú libre!")
+                alert("Opcion no válida");
                 break;
-        }
-        opcion = prompt("Ingrese día de la semana (Lunes a Domingo) para salir escribir EXIT");
+         }
+        
+        total = pago();
+        total = parseFloat(total)
     }
-}else {
-    alert("No reconoce el usuario.")
-}
-
-    
+  }
+  
+  metaMovimiento();
+  
